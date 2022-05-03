2a04f6333a5f5721038a1637974d4fea79a5d55f
Snehal1499 <snehalpatil1718@gmail.com>
2022-05-03 23:25:04 +0530

    Added  Welcome Message

---
+++ Task_Parallel_and_Multi_Threading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_Parallel_and_Multi_Threading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Task Parallel And Multi Threading Problem");
+        }
+    }
+}
