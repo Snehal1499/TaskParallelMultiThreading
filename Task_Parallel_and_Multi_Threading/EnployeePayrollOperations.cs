becaf4533f08cc8a14744bcdb9ca46e178767204
Snehal1499 <snehalpatil1718@gmail.com>
2022-05-03 23:34:22 +0530

    Added UC1 Time Duration

---
+++ Task_Parallel_and_Multi_Threading/EnployeePayrollOperations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Parallel_and_Multi_Threading
+{
+    public class EnployeePayrollOperations
+    {
+        public List<EmployeeDetails> employeePolyeeDetailList = new List<EmployeeDetails>();
+
+        //UC 1
+        public void addEmployeeToPayroll(List<EmployeeDetails> employeePayrollDataList)
+        {
+            employeePayrollDataList.ForEach(employeeData =>
+            {
+                Console.WriteLine("Employee Being Added :" + employeeData.EmployeeName);
+                this.addEmployeeToPayroll(employeeData);
+                Console.WriteLine("Employee added : " + employeeData.EmployeeName);
+            });
+            Console.WriteLine(this.employeePolyeeDetailList.ToString());
+        }
+        public void addEmployeeToPayroll(EmployeeDetails emp)
+        {
+            employeePolyeeDetailList.Add(emp);
+        }
+
+    }
+}
